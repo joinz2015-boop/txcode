d70d5a9363f31ea27c2673cd29048648ba91709c
wuxw <928255095@qq.com>
2026-04-16 20:21:50 +0800

    优化代码

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Text } from 'ink';
 
-const VERSION = '1.0.25';
+const VERSION = '1.0.26';
 
 export const Header = React.memo(function Header() {
 	return (
