0061a00abb765a98f18b12331a4263b1c9ce44f9
wuxw <928255095@qq.com>
2026-04-24 08:10:07 +0800

    发布1.0.34版本

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Text } from 'ink';
 
-const VERSION = '1.0.33';
+const VERSION = '1.0.34';
 
 export const Header = React.memo(function Header() {
 	return (
