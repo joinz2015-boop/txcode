1ddc0bf0822d4ff5c40526d6ff5e3119714df4ca
wuxw <928255095@qq.com>
2026-06-30 09:56:11 +0800

    完成spec的优化

--- src/modules/spec/spec.injector.ts
+++ src/modules/spec/spec.injector.ts
@@ -15,12 +15,6 @@ export class SpecInjector {
     
     const specs = this.specMgr.getLocalSpecs();
 
-    const requiredSpecs = specs.filter(s => s.read_mode === 'required');
-    const optionalSpecs = specs.filter(s => s.read_mode === 'optional');
-
-    if (requiredSpecs.length === 0 && optionalSpecs.length === 0) {
-      return '';
-    }
 
     let injection = `
 
@@ -30,30 +24,12 @@ export class SpecInjector {
 ## 规范说明
 你来分析一下这个问题是否为一个开发任务，如果是开发任务你必须要先阅读规范，然后严格按照规范开发，如果不是开发任务可以选择不读`;
 
-    if (requiredSpecs.length > 0) {
+    if (specs.length > 0) {
       injection += `
 
 ### 必读规范
 <specs>`;
-      for (const spec of requiredSpecs) {
-        const specPath = this.getSpecPath(spec.name, projectPath);
-        injection += `
-  <spec>
-    <name>${this.escapeXml(spec.name)}</name>
-    <description>${this.escapeXml(spec.description)}</description>
-    <path>${specPath}</path>
-  </spec>`;
-      }
-      injection += `
-</specs>`;
-    }
-
-    if (optionalSpecs.length > 0) {
-      injection += `
-
-### 选读规范
-<specs>`;
-      for (const spec of optionalSpecs) {
+      for (const spec of specs) {
         const specPath = this.getSpecPath(spec.name, projectPath);
         injection += `
   <spec>
